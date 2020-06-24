d0842994362f272bc305e441e9ef924cb973fd1c
2367018272 <2367018272@qq.com>
2020-06-24 20:53:35 +0800

    123124

--- BigHomeWork/text.py
+++
@@ -1,6 +0,0 @@
-import pymysql
-from getContent import getContent
-db = pymysql.connect("localhost", "root", "123456", "testdb")
-cursor = db.cursor()
-sql = "truncate table content"
-cursor.execute(sql)
